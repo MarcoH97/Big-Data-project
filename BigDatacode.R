702b92b6033cf8f073e030e12d4ae0bcc46dbf8c
MarcoH97 <marco1997925@hotmail.com>
2023-05-24 08:52:18 +0200

    update

--- BigDatacode.R
+++ BigDatacode.R
@@ -26,7 +26,7 @@ data <- read_excel("C:/Users/marco/Documents/HSG/Big Data/BDBD_data.xlsx")
 # Remove rows with NA
 data <- na.omit(data)
 
-data$Dates <- as.Date(data$Dates)
+#data$Dates <- as.Date(data$Dates)
 
 # Set the first column as row index
 #rownames(data) <- data[,1]
@@ -114,7 +114,7 @@ generate_weighted_cols_parallel <- function(data, max_comb_size) {
 
 
 start_time <- Sys.time()
-data <- generate_weighted_cols_parallel(data, 5)
+data <- generate_weighted_cols_parallel(data, 3)
 end_time <- Sys.time()
 execution_time <- end_time - start_time
 print(execution_time)
@@ -248,13 +248,19 @@ calculate_returns <- function(data, years, threshold) {
 
 # Use function
 # Replace "10" and "100" with your desired years and threshold
+start_time <- Sys.time()
 result <- calculate_returns(data, 10, 85)
+end_time <- Sys.time()
+execution_time <- end_time - start_time
+print(execution_time)
+
 
 # Print result
 print(result)
 
 
 #### Algo parallel processing ####
+
 calculate_returns_single_column <- function(col_name, data, period, threshold) {
   # Skip Date column
   if (col_name == "Date") return(NULL)
@@ -297,13 +303,17 @@ calculate_returns_parallel <- function(data, years, threshold) {
   # Convert years to trading days
   period <- years * days_per_year
   
+  # Create cluster with number of cores  
+  cl <- makeCluster(detectCores() - 1)
+  
+  # Export necessary objects to the workers
+  clusterExport(cl, c("data", "period", "threshold"))
+  
   # Apply the function to each column in parallel
-  output <- mclapply(names(data),
-                     calculate_returns_single_column,
-                     data = data,
-                     period = period,
-                     threshold = threshold,
-                     mc.cores = detectCores())
+  output <- parLapply(cl, names(data), calculate_returns_single_column)
+  
+  # Stop the cluster
+  stopCluster(cl)
   
   # Combine results and remove NULL results (columns that dropped below threshold)
   output <- do.call("rbind", output)
@@ -314,7 +324,12 @@ calculate_returns_parallel <- function(data, years, threshold) {
 
 # Use function
 # Replace "10" and "85" with your desired years and threshold
+start_time <- Sys.time()
 result <- calculate_returns_parallel(data, 10, 85)
+end_time <- Sys.time()
+execution_time <- end_time - start_time
+print(execution_time)
+
 
 # Print result
 print(result)
@@ -442,12 +457,14 @@ ggplot(data = melted_cormat, aes(x=Var1, y=Var2, fill=value)) +
 
 # PLot the mean-variance graph for the variables
 means <- colMeans(data)
-var <- apply(data, 2, var)
-# Convert variables to percentage
-means_percent <- means*100
-var_percent <- var*100
+daily_sd <- apply(data, 2, sd)
 
-plot_df <- data.frame(means=means_percent, sds=var_percent)
+# Annualizing means
+annual_means <- (1 + means)^252 - 1
+# Annualize the standard deviation
+annual_sd <- daily_sd * sqrt(252)
+
+plot_df <- data.frame(means=annual_means, sds=annual_sd)
 
 # Add a new column for color
 plot_df$group <- NA
@@ -460,9 +477,9 @@ plot_df$group[17902:83681] <- '5 combinations'
 
 
 # Plot with color aesthetic mapped to group
-ggplot(plot_df, aes(x=means_percent, y = var_percent, color=group))+
+ggplot(plot_df, aes(x=annual_means, y = annual_sd, color=group))+
   geom_point()+
-  labs(x='Mean', y="Variance", title="Mean and variance of all the investment strategies")+
+  labs(x='Mean', y="Standard deviation", title="Annualised mean and standard deviation of all the investment strategies")+
   scale_x_continuous(labels = scales::percent)+
   scale_y_continuous(labels = scales::percent)+
   scale_color_manual(values=c('Original strategies' = '#000000', '2 combinations' = '#FF0000', 
@@ -485,4 +502,41 @@ ggplot(plot_df, aes(x=means_percent, y = var_percent))+
   theme(plot.title = element_text(size= 14, hjust = 0.5, face ="bold"), 
         axis.text.x = element_text(angle = 90, hjust = 1, face = "bold"),
         axis.text.y = element_text(angle = 90, hjust = 1, face = "bold"))
-  
+
+
+# Plot the runtime differences
+# Create the data
+combinations <- c(2, 3, 4, 5)
+normal_runtime <- c(0.001279648, 0.018118033, 0.231115833, 8.696434)
+#c(0.07677889, 1.087082, 13.86695, 521.78604)
+parallel_runtime <- c(0.019332633, 0.061772267, 0.342696167, 1.232462)
+#c(1.159958, 3.706336, 20.56177, 73.94772)
+
+runtime_df <- data.frame(Combinations = combinations, Normal_Runtime = normal_runtime, Parallel_Runtime = parallel_runtime)
+
+# Plotting the data
+# Define the desired legend title and labels
+legend_title <- "Type of generation"
+legend_labels <- c("Normal Runtime", "Parallel Runtime")
+
+ggplot(runtime_df, aes(x = Combinations)) +
+  geom_line(aes(y = Normal_Runtime, color = "Normal R"), size = 1) +
+  geom_line(aes(y = Parallel_Runtime, color = "Parallel Processing"), size = 1) +
+  labs(title = "Runtime Comparison for data generation",
+       x = "Combinations",
+       y = "Runtime (minuites)",
+       color = legend_title) +
+  theme_bw() +
+  theme(plot.title =  element_text(size= 14, hjust = 0.5, face ="bold"), 
+        axis.text.x = element_text(angle = 90, hjust = 1, face = "bold"),
+        axis.text.y = element_text(angle = 90, hjust = 1, face = "bold"),
+        legend.position = "right")
+
+
+# Create the data
+combinations2 <- c(2, 3, 4, 5, 6, 7, 8, 9, 10)
+columns2 <- c(351, 2951, 17901, 83681, 313911, 971711, 2533986, 5658536, 10970271)
+file_size2 <- c(14, 115, 694, 3245, 12171, 37675, 98248, 219394, 425342)
+
+# Create the dataframe
+size_df <- data.frame(Combinations = combinations2, Columns = columns2, File_Size_MB = file_size2)
